6119361c01a8582eda111780cda191cfb52b7072
dhanesambar <ambardhanes@gmail.com>
2017-03-06 07:14:40 +0700

    fixing

--- kontentengah-member.php
+++ kontentengah-member.php
@@ -153,10 +153,11 @@
     <!-- end:cta -->
 
     <!-- Notice Alert -->
-
     <?php 
       if ($_SESSION['pesan_sukses']!='') {
         echo "<script> alert('".$_SESSION['pesan_sukses']."') </script>";
         unset($_SESSION['pesan_sukses']);
       }
     ?>
+    <!-- //Notice Alert -->
+    fsdkfjsdkjfkdsjfkdsjfkdsjfkdsjf
